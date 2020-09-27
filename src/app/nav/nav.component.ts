53d10324050ad6c4cb2290e291e7ef68ab94e96b
vimesh-shah <vimesh93@gmail.com>
2020-09-27 19:31:39 +0530

    3 files modified

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChildren } from "@angular/core";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
+  @ViewChildren("navlink") navlinks;
 
-  constructor() { }
+  isNavActive = false;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  burgerClicked() {
+    this.isNavActive = !this.isNavActive;
+    this.navlinks.toArray().forEach((link, index) => {
+      link.nativeElement.style.animation =
+        "navLinkFade 0.5s ease forwards " + (index / 7 + 0.3) + "s";
+    });
   }
-
 }
