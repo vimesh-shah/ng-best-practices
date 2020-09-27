d2ee074f0f57113cef67009c90e78c099904544e
vimesh-shah <vimesh93@gmail.com>
2020-09-27 18:28:35 +0530

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
